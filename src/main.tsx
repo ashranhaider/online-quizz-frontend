81495172c7d9343787622ccb07d8e588a4868f4f
ashranhaider <ch.ashran@gmail.com>
2025-11-28 13:02:39 -0500

    routes to dedicated file

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
-import App from './App.tsx'
-import Home from './pages/Home.tsx';
-import Login from './pages/auth/login.tsx';
-import Register from './pages/auth/register.tsx';
+import AppRoutes from './app-routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />         
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
 );
